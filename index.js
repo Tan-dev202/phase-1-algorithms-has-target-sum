80acada797ff31a14021f64e1c70801c44da07b7
Enock Tangus <tanguskipngeno@gmail.com>
2025-03-23 11:18:38 +0300

    algorithms has target sum lab complete

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const checkedNumbers = {};
+  for (const num of array) {
+    const compareNum = target - num;
+    if (compareNum in checkedNumbers) return true;
+    checkedNumbers[num] = compareNum;
+  }
+  return false;
 }
+
+console.log(hasTargetSum([28, 55, 24, 69], 83));
 
 /* 
   Write the Big O time complexity of your function here
+*add to obj => O(1)
+*for loop => O(n)
+*obj lookup and assign => O(1)
+Big O time complexity of the function is O(n)
 */
 
 /* 
   Add your pseudocode here
+*create obj to add the numbers
+*subtract a num from the target to compare with any other in obj
+*if result match a num in obj, return true, otherwise return false
 */
 
 /*
   Add written explanation of your solution here
+  *the function creates a checkedNumbers object to add the numbers
+  *loops thro the array and gets diff between the num and target
+  *compares the diff with a num in the obj, then return true if matching, else returns false
 */
 
 // You can run `node index.js` to view these console logs
